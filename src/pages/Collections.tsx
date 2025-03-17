b48c8cf8f2ed44b3d273786946ce9d481506050f
Sagar <129282569+sagar1343@users.noreply.github.com>
2025-03-17 10:02:30 +0530

    Feature (#3)
    * added context menu
    
    * added delete collection functionality
    
    * added rename collection functionality
    
    * fix collection layout and button alignment issue in dialog
    
    * Avoid duplicate title error when renaming collection
    
    * update collection
    
    * improve deselect
    
    ---------
    
    Co-authored-by: Rohit3124 <rohitkumawat4493@gmail.com>

--- src/pages/Collections.tsx
+++ src/pages/Collections.tsx
@@ -5,13 +5,29 @@ import Heading from "@/components/Heading";
 import Loader from "@/components/Loader";
 import { useCollections } from "@/context/CollectionsContext";
 import clsx from "clsx";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Collections() {
   const { collections, isLoading, isCreating } = useCollections();
-  const [active, setActive] = useState(-1);
+  const [active, setActive] = useState<number | null>(null);
+  const containerRef = useRef<HTMLUListElement | null>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        containerRef.current &&
+        !containerRef.current.contains(event.target as Node)
+      ) {
+        setActive(null);
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+    return () => document.removeEventListener("click", handleClickOutside);
+  }, []);
 
   if (isLoading) return <Loader />;
+
   return (
     <>
       <div>
@@ -19,7 +35,10 @@ export default function Collections() {
           Collections <CreateCollectionButton />
         </Heading>
       </div>
-      <ul className="mt-12 grid grid-cols-3 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-6 justify-items-center gap-6">
+      <ul
+        ref={containerRef}
+        className="mt-12 grid grid-cols-3 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-6 justify-items-center gap-6"
+      >
         {isCreating && (
           <li>
             <CollectionInput />
@@ -29,10 +48,16 @@ export default function Collections() {
         {collections.map((collection) => (
           <li
             key={collection.id}
-            onClick={() => setActive(collection.id)}
-            className={clsx("rounded-md", {
-              "bg-white/20": active === collection.id,
-            })}
+            onClick={(e) => {
+              e.stopPropagation();
+              setActive(active === collection.id ? null : collection.id);
+            }}
+            className={clsx(
+              "rounded-md cursor-pointer transition-all",
+              active === collection.id
+                ? "bg-white/20 shadow-lg"
+                : "bg-transparent"
+            )}
           >
             <Collection collection={collection} />
           </li>
