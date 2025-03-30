ea9934cc15f32f170cea7825ab90b6089a22d8cb
Sagar Chakrawarti <sagarchakrawarti25@gmail.com>
2025-03-30 22:07:37 +0530

    solved issue #7

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,10 +1,12 @@
 import { useAuthContext } from "@/context/AuthContext";
 import { ReactNode } from "react";
-import { Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Loader from "./Loader";
 
 export default function ProtectedRoute({ children }: { children: ReactNode }) {
   const { loading, isAuthenticated } = useAuthContext();
+  const navigate = useNavigate();
   if (loading) return <Loader />;
-  return isAuthenticated ? <>{children}</> : <Navigate to="/login" />;
+  if (!isAuthenticated) navigate("/login");
+  return children;
 }
