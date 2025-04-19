c6d910854d3a69f03e98da8563d4c3c3cf91787a
Sagar Chakrawarti <sagarchakrawarti25@gmail.com>
2025-04-19 19:11:15 +0530

    renoved unused item

--- src/components/GoogleLoginButton.tsx
+++ src/components/GoogleLoginButton.tsx
@@ -4,11 +4,9 @@ import {
   GoogleLogin,
   GoogleOAuthProvider,
 } from "@react-oauth/google";
-import { useNavigate } from "react-router-dom";
 
 export default function GoogleLoginButton() {
   const { login } = useAuthContext();
-  const navigate = useNavigate();
 
   return (
     <GoogleOAuthProvider clientId="1085027911555-pbbdr6onm4864g8353p0a0oabr9nlp39.apps.googleusercontent.com">
