a831531532c6d887762f2d6479b10fc2637ea56a
Sagar Chakrawarti <sagarchakrawarti25@gmail.com>
2025-05-26 14:04:00 +0530

    updated api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,7 @@
 import axios from "axios";
 
-// const url = "http://15.206.128.181";
-const dev_url = "http://localhost:8000";
-
 const api = axios.create({
-  baseURL: dev_url,
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
